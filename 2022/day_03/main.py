3da7566f0f4ce04ef7c8afd00365dc1869174a50
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2022-12-05 16:33:44 +0100

    day 3 done

--- 2022/day_03/main.py
+++ 2022/day_03/main.py
@@ -9,6 +9,49 @@ def read_file(fp):
     return data
 
 
+def intersect(*sets):
+    for item in sets[0]:
+        common = True
+        for set_i in sets[1::]:
+            common = common and (item in set_i)
+
+        if common:
+            return item
+
+
+def get_priority(item):
+    if item.islower():
+        # a = 1
+        return ord(item) - 96
+
+    elif item.isupper():
+        # A = 27
+        return ord(item) - 38
+
+
+def part_1(data):
+    priority = 0
+    for rucksack in data:
+        size = len(rucksack)
+        comp1, comp2 = rucksack[:size//2], rucksack[size//2::]
+        common = intersect(comp1, comp2)
+        priority += get_priority(common)
+
+    return priority
+
+
+def part_2(data):
+    priority = 0
+    for i in range(0, len(data), 3):
+        r1, r2, r3 = data[i], data[i+1], data[i+2]
+        common = intersect(r1, r2, r3)
+        p = get_priority(common)
+        priority += p
+
+    return priority
+
+
 if __name__ == "__main__":
-    data = read_file("test_input.txt")
-    print(data)
+    data = read_file("input.txt")
+    print(f"Part 1:\n{part_1(data)}")
+    print(f"Part 2:\n{part_2(data)}")
